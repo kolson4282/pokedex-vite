7c52d9dbce6fd31a4bcca73487ac0817d54cc5ca
kolson <kolson@dmsi.com>
2022-06-16 15:51:09 -0500

    basic pokemon card getting data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+
+const starters = ["bulbasaur", "charmander", "squirtle"];
+const queryClient = new QueryClient();
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {starters.map((pkmn) => (
+        <PokemonCard key={pkmn} name={pkmn} />
+      ))}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,29 @@
+import { useQuery } from "react-query";
+
+type Props = {
+  name: string;
+};
+
+const PokemonCard = ({ name }: Props) => {
+  const pokemon = useQuery(`${name}-card`, () => {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((res) =>
+      res.json()
+    );
+  });
+
+  if (pokemon.isLoading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <div>{pokemon.data?.id}</div>
+      <img
+        style={{ width: "10em" }}
+        src={pokemon.data?.sprites.other["official-artwork"].front_default}
+        alt={name}
+      />
+      <div>{name}</div>
+    </>
+  );
+};
+
+export default PokemonCard;
