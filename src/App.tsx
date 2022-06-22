049fd9a9c58d7142e2d13c301c677fc16836deea
kolson <kolson@dmsi.com>
2022-06-22 12:52:41 -0500

    implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,21 +3,28 @@ import { useQuery } from "react-query";
 import PokemonCard from "../components/PokemonCard/PokemonCard";
 
 const Home = () => {
-  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [url, setUrl] = useState(
+    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=0`
+  );
 
   const pokemon = useQuery(url, () => {
     return fetch(url).then((res) => res.json());
   });
+
+  const previous = () => {
+    setUrl(pokemon.data.previous);
+  };
+
+  const next = () => {
+    setUrl(pokemon.data.next);
+  };
+
   if (pokemon.isLoading) return <p>...Loading</p>;
 
   return (
     <>
-      {!pokemon.data.previous || (
-        <button onClick={() => setUrl(pokemon.data.previous)}>Prev</button>
-      )}
-      {!pokemon.data.next || (
-        <button onClick={() => setUrl(pokemon.data.next)}>Next</button>
-      )}
+      {!pokemon.data.previous || <button onClick={previous}>Prev</button>}
+      {!pokemon.data.next || <button onClick={next}>Next</button>}
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {pokemon?.data.results.map((pkmn: { name: string }) => (
           <PokemonCard key={pkmn.name} name={pkmn.name} />
